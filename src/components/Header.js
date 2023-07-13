2e19cfff2d325f81a2d8025db8426f39a0fed16e
Antonio Martinez Perez <antonio.martinez.p@ricoh.es>
2023-07-13 12:42:37 +0200

    feat: (header) - Changed title page

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <nav className="navbar navbar-expand-sm navbar navbar-dark bg-dark">
       <div className="container-md">
-        <Link className="navbar-brand" to="/">React Tests</Link>
+        <Link className="navbar-brand" to="/">React Wiki</Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
           aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
