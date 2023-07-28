bfd3a2b5e22f7d6de11bac3c1757ef6b31aa473b
Antonio Martinez Perez <antonio.martinez.p@ricoh.es>
2023-07-28 12:03:36 +0200

    feat: (movile design, navigation) - Removed bootstrap module - Added bootsrap by CDN links - Fixed hamburguer button - Added some styles app.css - Added new section react hooks

--- src/App.js
+++ src/App.js
@@ -36,6 +36,7 @@ function App() {
           </ImagesContextProvider>
         </div>
         <Footer />
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossOrigin="anonymous"></script>
       </div>
   );
 }
