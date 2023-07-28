08e783f63a580bb439079fe7109234c8a68ca4a5
Antonio Martinez Perez <antonio.martinez.p@ricoh.es>
2023-07-28 12:20:24 +0200

    feat: (footer) - updated footer links from header

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import { Link } from "react-router-dom";
 
@@ -7,14 +8,31 @@ const Footer = () => {
       <div className="container">
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
-            <li className="nav-item">
-              <Link className="nav-link" to="/use-effect">useEffect & ModuleCSS</Link>
+          <li className="nav-item dropup">
+              <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                React Hooks
+              </a>
+              <ul className="dropdown-menu dropdown">
+                <li>
+                 <Link className="dropdown-item" to="/use-effect">useEffect & ModuleCSS</Link>
+                </li>
+                <li>
+                 <Link className="dropdown-item" to="/use-effect">useReducer</Link>
+                </li>
+              </ul>
             </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/links">Links</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/context">Contextos Rutas dinámicas</Link>
+            <li className="nav-item dropup">
+              <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Links
+              </a>
+              <ul className="dropdown-menu dropdown">
+                <li>
+                 <Link className="dropdown-item" to="/links">Enlaces simples</Link>
+                </li>
+                <li>
+                 <Link className="dropdown-item" to="/context">Contextos Rutas dinámicas</Link>
+                </li>
+              </ul>
             </li>
             <li className="nav-item">
               <Link className="nav-link" to="/lazy-component">Lazy Component</Link>
