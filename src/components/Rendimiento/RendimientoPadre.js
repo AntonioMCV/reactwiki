6126c3baaefb65da930ee845daba32ac0f8bf240
Antonio Martinez Perez <antonio.martinez.p@ricoh.es>
2023-08-07 10:33:20 +0200

    feat: (warnings) - Fixed warnings

--- src/components/Rendimiento/RendimientoPadre.js
+++ src/components/Rendimiento/RendimientoPadre.js
@@ -1,43 +1,59 @@
-import React, { useCallback, useState, useMemo, useEffect } from "react";
+import React, { useCallback, useState, useMemo } from "react";
 import RendimientoHijo from "./RendimientoHijo";
 
-export default function RendimientoPadre () {
-  console.log('COMPONENTE PADRE RENDER!');
+export default function RendimientoPadre() {
+  console.log("COMPONENTE PADRE RENDER!");
 
-  const [keyWord, setKeyWord] = useState('')
-  const [nombreHijo, setNombreHijo] = useState('hijo')
-  const [nombrePadre, setNombrePadre] = useState('padre')
+  const [keyWord, setKeyWord] = useState("");
+  const [nombreHijo, setNombreHijo] = useState("hijo");
+  const [nombrePadre, setNombrePadre] = useState("padre");
 
   const handleSubmit = (evt) => {
-    evt.preventDefault()
-    setNombreHijo(keyWord)
-  }
+    evt.preventDefault();
+    setNombreHijo(keyWord);
+  };
 
   const handleChange = useCallback((evt) => {
-    setKeyWord(evt.target.value)
-  }, [])
+    setKeyWord(evt.target.value);
+  }, []);
 
   const cambiarNombrePadre = useCallback(
-    (newName) => setNombrePadre(newName),[]
-  )
+    (newName) => setNombrePadre(newName),
+    []
+  );
 
-  const numbers = useMemo(()=>[1,2,3,4], [])
+  const numbers = useMemo(() => [1, 2, 3, 4], []);
 
-  return <>
+  return (
+    <>
       <div className="border border-black p-5 mb-5">
         <h2 className="mb-4">Componente {nombrePadre}</h2>
         <form onSubmit={handleSubmit}>
           <div className="row align-items-end mb-4">
             <div className="col">
-                <label className="form-label">Escribe para volver a renderizar el componente padre</label>
-                <input className="form-control" type="text" onChange={handleChange} value={keyWord}/>
+              <label className="form-label">
+                Escribe para volver a renderizar el componente padre
+              </label>
+              <input
+                className="form-control"
+                type="text"
+                onChange={handleChange}
+                value={keyWord}
+              />
             </div>
             <div className="col-auto">
-              <button className="btn btn-primary">Cambiar el nombre del hijo</button>
+              <button className="btn btn-primary">
+                Cambiar el nombre del hijo
+              </button>
             </div>
           </div>
         </form>
-        <RendimientoHijo changeName={nombreHijo} cambiarNombrePadre={cambiarNombrePadre} exampleArray={numbers}/>
+        <RendimientoHijo
+          changeName={nombreHijo}
+          cambiarNombrePadre={cambiarNombrePadre}
+          exampleArray={numbers}
+        />
       </div>
-  </>
+    </>
+  );
 }
--- src/pages/librerias/redux/BasicoPage.js
+++ src/pages/librerias/redux/BasicoPage.js
@@ -1,4 +1,4 @@
-import Counter from "components/ReduxCounter/Counter";
+import Counter from "components/ReduxCounterBasico/Counter";
 import { Link } from "react-router-dom";
 
 const BasicoPage = () => {
@@ -17,43 +17,43 @@ const BasicoPage = () => {
           complicado
         </b>
         . Aquí es donde <b>Redux</b> entra en juego para ayudarte:
-        <br />
-        <br />
-        <ul>
-          <li>
-            <b>Almacén (Store)</b>: Piensa en el almacén de Redux como un gran
-            contenedor para todos los estados importantes de tu aplicación. Es
-            como un lugar central donde guardas información relevante, como la
-            información del usuario, las publicaciones, etc.
-          </li>
-          <li>
-            <b>Acciones (Actions)</b>: Las acciones en Redux son como
-            solicitudes para cambiar algo en tu almacén. Por ejemplo, si alguien
-            hace clic en un botón "Me gusta", eso genera una "acción" para
-            aumentar el contador de "Me gusta". Las acciones son pequeños
-            objetos que describen qué sucedió.
-          </li>
-          <li>
-            <b>Reductores (Reducers)</b>: Los reductores son como las personas
-            encargadas de procesar esas acciones y hacer cambios en el almacén.
-            Toman la acción y actualizan el estado en función de lo que debería
-            cambiar. Por ejemplo, un reductor tomará la acción de "Aumentar Me
-            gusta" y ajustará el número de Me gusta en el estado del almacén.{" "}
-          </li>
-          <li>
-            <b>Despachar (Dispatch)</b>: Es como decirle a Redux que ocurrió
-            algo y que debe manejarlo. Por ejemplo, cuando alguien hace clic en
-            un botón, puedes "despachar" una acción, que luego será manejada por
-            los reductores para actualizar el estado en el almacén.{" "}
-          </li>
-          <li>
-            <b>Suscribirse (Subscribe)</b>: Puedes "suscribir" componentes
-            específicos al almacén de Redux. Esto significa que esos componentes
-            serán notificados cada vez que el estado en el almacén cambie. De
-            esta manera, puedes actualizar automáticamente la interfaz de
-            usuario para que coincida con el estado actual.{" "}
-          </li>
-        </ul>
+      </p>
+      <ul>
+        <li>
+          <b>Almacén (Store)</b>: Piensa en el almacén de Redux como un gran
+          contenedor para todos los estados importantes de tu aplicación. Es
+          como un lugar central donde guardas información relevante, como la
+          información del usuario, las publicaciones, etc.
+        </li>
+        <li>
+          <b>Acciones (Actions)</b>: Las acciones en Redux son como solicitudes
+          para cambiar algo en tu almacén. Por ejemplo, si alguien hace clic en
+          un botón "Me gusta", eso genera una "acción" para aumentar el contador
+          de "Me gusta". Las acciones son pequeños objetos que describen qué
+          sucedió.
+        </li>
+        <li>
+          <b>Reductores (Reducers)</b>: Los reductores son como las personas
+          encargadas de procesar esas acciones y hacer cambios en el almacén.
+          Toman la acción y actualizan el estado en función de lo que debería
+          cambiar. Por ejemplo, un reductor tomará la acción de "Aumentar Me
+          gusta" y ajustará el número de Me gusta en el estado del almacén.{" "}
+        </li>
+        <li>
+          <b>Despachar (Dispatch)</b>: Es como decirle a Redux que ocurrió algo
+          y que debe manejarlo. Por ejemplo, cuando alguien hace clic en un
+          botón, puedes "despachar" una acción, que luego será manejada por los
+          reductores para actualizar el estado en el almacén.{" "}
+        </li>
+        <li>
+          <b>Suscribirse (Subscribe)</b>: Puedes "suscribir" componentes
+          específicos al almacén de Redux. Esto significa que esos componentes
+          serán notificados cada vez que el estado en el almacén cambie. De esta
+          manera, puedes actualizar automáticamente la interfaz de usuario para
+          que coincida con el estado actual.{" "}
+        </li>
+      </ul>
+      <p>
         En resumen, Redux trabaja en conjunto con React para ayudarte a manejar
         el estado de tu aplicación de una manera más organizada y predecible. Te
         brinda una estructura clara para gestionar cambios en el estado, lo que
@@ -62,24 +62,26 @@ const BasicoPage = () => {
         <br />
         <br />
         Para poder utilizarlo debemos:
-        <ul>
-          <li>
-            <b>Instalarnos</b> los paquetes npm <b>redux y react-redux</b>
-          </li>
-          <li>
-            <b>Importar el Provider</b> Redux y <b>envolver los componentes</b>.
-          </li>
-          <li>
-            <b>
-              Crear nuestra Store y pasarla como atributo en el componente
-              Provider
-            </b>
-          </li>
-          <li>
-            <b>Usar useSelector, useDispatch</b> de react-redux en los
-            componentes donde queramos cambiar el estado de Redux
-          </li>
-        </ul>
+      </p>
+      <ul>
+        <li>
+          <b>Instalarnos</b> los paquetes npm <b>redux y react-redux</b>
+        </li>
+        <li>
+          <b>Importar el Provider</b> Redux y <b>envolver los componentes</b>.
+        </li>
+        <li>
+          <b>
+            Crear nuestra Store y pasarla como atributo en el componente
+            Provider
+          </b>
+        </li>
+        <li>
+          <b>Usar useSelector, useDispatch</b> de react-redux en los componentes
+          donde queramos cambiar el estado de Redux
+        </li>
+      </ul>
+      <p>
         Como nota importante saber que nuestros Reducers siempre deben retornar
         el objeto entero de nuevo, esto puede llevar a que nuestro archivo sea
         demasiado grande y dificil de mantener es por eso que ahora tenemos a
