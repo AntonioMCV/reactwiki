0390399d186a83535e44722c15654c619846dcbd
Antonio Martinez Perez <antonio.martinez.p@ricoh.es>
2023-08-11 12:44:30 +0200

    feat: (translations) - Changed translations structure

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ import { useTranslation } from "react-i18next"
 const Header = () => {
   const [showMenuResponsive, setShowMenuResponsive] = useState(false)
   const hamburguerClickHandler = () => setShowMenuResponsive(prevState => !prevState)
-  const {t, i18n} = useTranslation(["navigation"])
+  const {t, i18n} = useTranslation(["components"])
 
   const changeLanguage = () => {
     i18n.changeLanguage(i18n.language === 'en'? 'es' : 'en')
@@ -26,62 +26,62 @@ const Header = () => {
           <ul className="navbar-nav mr-auto">
             <li className="nav-item dropdown">
               <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                {t('reactHooks')}
+                {t('navigation.reactHooks')}
               </a>
               <ul className="dropdown-menu dropdown-menu-dark">
                 <li>
-                 <Link className="dropdown-item" to="/hooks/use-effect">{t('useEffect')}</Link>
+                 <Link className="dropdown-item" to="/hooks/use-effect">{t('navigation.useEffect')}</Link>
                 </li>
                 <li>
-                  <Link className="dropdown-item" to="/hooks/use-memo-use-callback">{t('useMemoAndUseCallback')}</Link>
+                  <Link className="dropdown-item" to="/hooks/use-memo-use-callback">{t('navigation.useMemoAndUseCallback')}</Link>
                 </li>
                 <li>
-                 <Link className="dropdown-item" to="/hooks/use-reducer">{t('useReducer')}</Link>
+                 <Link className="dropdown-item" to="/hooks/use-reducer">{t('navigation.useReducer')}</Link>
                 </li>
               </ul>
             </li>
             <li className="nav-item dropdown">
               <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                {t('libraries')}
+                {t('navigation.libraries')}
               </a>
               <ul className="dropdown-menu dropdown-menu-dark">
                 <li className="dropdown-item active">
-                  {t('redux')}
+                  {t('navigation.redux')}
                 </li>
                 <li>
-                  <Link className="dropdown-item" to="librerias/redux/basico">{t('reduxBasic')}</Link>
+                  <Link className="dropdown-item" to="librerias/redux/basico">{t('navigation.reduxBasic')}</Link>
                 </li>
                 <li>
-                  <Link className="dropdown-item" to="librerias/redux/toolkit">{t('reduxToolkit')}</Link>
+                  <Link className="dropdown-item" to="librerias/redux/toolkit">{t('navigation.reduxToolkit')}</Link>
                 </li>
                 <li><hr className="dropdown-divider"/></li>
                 <li className="dropdown-item active">
-                  {t('routerDom')}
+                  {t('navigation.routerDom')}
                 </li>
                 <li>
-                  <Link className="dropdown-item" to="/librerias/router-dom/dynamic-links">{t('dynamicRoutes')}</Link>
+                  <Link className="dropdown-item" to="/librerias/router-dom/dynamic-links">{t('navigation.dynamicRoutes')}</Link>
                 </li>
                 <li>
-                  <Link className="dropdown-item" to="/librerias/router-dom/simple-links">{t('simpleRoutes')}</Link>
+                  <Link className="dropdown-item" to="/librerias/router-dom/simple-links">{t('navigation.simpleRoutes')}</Link>
                 </li>
               </ul>
             </li>
             <li className="nav-item dropdown">
               <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                {t('others')}
+                {t('navigation.others')}
               </a>
               <ul className="dropdown-menu dropdown-menu-dark">
                 <li>
-                  <Link className="dropdown-item" to="/otros/apis">{t('apis')}</Link>
+                  <Link className="dropdown-item" to="/otros/apis">{t('navigation.apis')}</Link>
                 </li>
                 <li>
-                  <Link className="dropdown-item" to="/otros/context">{t('contexts')}</Link>
+                  <Link className="dropdown-item" to="/otros/context">{t('navigation.contexts')}</Link>
                 </li>
                 <li>
-                  <Link className="dropdown-item" to="/otros/module-css">{t('moduleCSS')}</Link>
+                  <Link className="dropdown-item" to="/otros/module-css">{t('navigation.moduleCSS')}</Link>
                 </li>
                 <li>
-                  <Link className="dropdown-item" to="/otros/lazy-component">{t('lazyComponent')}</Link>
+                  <Link className="dropdown-item" to="/otros/lazy-component">{t('navigation.lazyComponent')}</Link>
                 </li>
               </ul>
             </li>
@@ -89,7 +89,7 @@ const Header = () => {
           <ul className="navbar-nav ml-auto">
             <li className="nav-item dropdown">
               <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                {t('language')}: {i18n.language.toUpperCase()}
+                {t('navigation.language')}: {i18n.language.toUpperCase()}
               </a>
               <ul className="dropdown-menu dropdown-menu-dark">
                 <li>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,15 +1,15 @@
 import { useTranslation } from "react-i18next"
 
 const HomePage = () => {
-  const {t} = useTranslation(["home"])
+  const {t} = useTranslation(["pages"])
 
   return (
     <>
-      <h1 className="bg-dark text-white text-center my-4 py-4">{t('reactWiki')}</h1>
+      <h1 className="bg-dark text-white text-center my-4 py-4">{t('home.reactWiki')}</h1>
 
-      <p className="alert alert-warning" dangerouslySetInnerHTML={{__html: t('warningBox')}}/>
-      <h2 className="text-center mb-5">{t('introduction')}</h2>
-      <h5 className="text-center" dangerouslySetInnerHTML={{__html: t('introductionDetails')}}/>
+      <p className="alert alert-warning" dangerouslySetInnerHTML={{__html: t('home.warningBox')}}/>
+      <h2 className="text-center mb-5">{t('home.introduction')}</h2>
+      <h5 className="text-center" dangerouslySetInnerHTML={{__html: t('home.introductionDetails')}}/>
       <div className="text-center mb-5 fs-5">
         <ul className="d-inline-block">
           <li>
@@ -18,7 +18,7 @@ const HomePage = () => {
               target="_blank"
               rel="noreferrer"
             >
-              {t('gitHub')}
+              {t('home.gitHub')}
             </a>
           </li>
           <li>
@@ -27,15 +27,15 @@ const HomePage = () => {
               target="_blank"
               rel="noreferrer"
             >
-              {t('sandBox')}
+              {t('home.sandBox')}
             </a>
           </li>
         </ul>
       </div>
 
       <div className="alert alert-secondary">
-        <h3 className="mb-4">{t('infoBoxTitle')}</h3>
-        <p dangerouslySetInnerHTML={{__html: t('infoBox')}}/>
+        <h3 className="mb-4">{t('home.infoBoxTitle')}</h3>
+        <p dangerouslySetInnerHTML={{__html: t('home.infoBox')}}/>
       </div>
     </>
   );
