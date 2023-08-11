6169aaadea526aae9dc6dc2b14ed56cf042df70d
Antonio Martinez Perez <antonio.martinez.p@ricoh.es>
2023-08-11 12:14:55 +0200

    feat: (translations) - Added navigation translations - Added home translations

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,17 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, {useState} from "react";
 import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next"
 
 
 const Header = () => {
   const [showMenuResponsive, setShowMenuResponsive] = useState(false)
   const hamburguerClickHandler = () => setShowMenuResponsive(prevState => !prevState)
+  const {t, i18n} = useTranslation(["navigation"])
+
+  const changeLanguage = () => {
+    i18n.changeLanguage(i18n.language === 'en'? 'es' : 'en')
+  }
 
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -16,66 +22,78 @@ const Header = () => {
           <span className="navbar-toggler-icon"></span>
         </button>
 
-        <div className={`collapse navbar-collapse ${showMenuResponsive? 'show':''}`} id="navbarSupportedContent">
+        <div className={`collapse navbar-collapse justify-content-between ${showMenuResponsive? 'show':''}`} id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
             <li className="nav-item dropdown">
               <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                React Hooks
+                {t('reactHooks')}
               </a>
               <ul className="dropdown-menu dropdown-menu-dark">
                 <li>
-                 <Link className="dropdown-item" to="/hooks/use-effect">useEffect</Link>
+                 <Link className="dropdown-item" to="/hooks/use-effect">{t('useEffect')}</Link>
                 </li>
                 <li>
-                  <Link className="dropdown-item" to="/hooks/use-memo-use-callback">useMemo y useCallback</Link>
+                  <Link className="dropdown-item" to="/hooks/use-memo-use-callback">{t('useMemoAndUseCallback')}</Link>
                 </li>
                 <li>
-                 <Link className="dropdown-item" to="/hooks/use-reducer">useReducer</Link>
+                 <Link className="dropdown-item" to="/hooks/use-reducer">{t('useReducer')}</Link>
                 </li>
               </ul>
             </li>
             <li className="nav-item dropdown">
               <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Librerías
+                {t('libraries')}
               </a>
               <ul className="dropdown-menu dropdown-menu-dark">
                 <li className="dropdown-item active">
-                  Redux
+                  {t('redux')}
                 </li>
                 <li>
-                  <Link className="dropdown-item" to="librerias/redux/basico">Redux básico</Link>
+                  <Link className="dropdown-item" to="librerias/redux/basico">{t('reduxBasic')}</Link>
                 </li>
                 <li>
-                  <Link className="dropdown-item" to="librerias/redux/toolkit">Redux toolkit</Link>
+                  <Link className="dropdown-item" to="librerias/redux/toolkit">{t('reduxToolkit')}</Link>
                 </li>
                 <li><hr className="dropdown-divider"/></li>
                 <li className="dropdown-item active">
-                  Router Dom
+                  {t('routerDom')}
                 </li>
                 <li>
-                  <Link className="dropdown-item" to="/librerias/router-dom/dynamic-links">Rutas dinámicas</Link>
+                  <Link className="dropdown-item" to="/librerias/router-dom/dynamic-links">{t('dynamicRoutes')}</Link>
                 </li>
                 <li>
-                  <Link className="dropdown-item" to="/librerias/router-dom/simple-links">Enlaces simples</Link>
+                  <Link className="dropdown-item" to="/librerias/router-dom/simple-links">{t('simpleRoutes')}</Link>
                 </li>
               </ul>
             </li>
             <li className="nav-item dropdown">
               <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Otros
+                {t('others')}
               </a>
               <ul className="dropdown-menu dropdown-menu-dark">
                 <li>
-                  <Link className="dropdown-item" to="/otros/apis">APIs</Link>
+                  <Link className="dropdown-item" to="/otros/apis">{t('apis')}</Link>
                 </li>
                 <li>
-                  <Link className="dropdown-item" to="/otros/context">Contextos</Link>
+                  <Link className="dropdown-item" to="/otros/context">{t('contexts')}</Link>
                 </li>
                 <li>
-                  <Link className="dropdown-item" to="/otros/module-css">ModuleCSS</Link>
+                  <Link className="dropdown-item" to="/otros/module-css">{t('moduleCSS')}</Link>
                 </li>
                 <li>
-                  <Link className="dropdown-item" to="/otros/lazy-component">Lazy Component</Link>
+                  <Link className="dropdown-item" to="/otros/lazy-component">{t('lazyComponent')}</Link>
+                </li>
+              </ul>
+            </li>
+          </ul>
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                {t('language')}: {i18n.language}
+              </a>
+              <ul className="dropdown-menu dropdown-menu-dark">
+                <li>
+                 <button className="dropdown-item" onClick={changeLanguage}>{i18n.language === 'en'? 'es' : 'en'}</button>
                 </li>
               </ul>
             </li>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,29 +1,16 @@
+import { useTranslation } from "react-i18next"
+
 const HomePage = () => {
+  const {t} = useTranslation(["home"])
+
   return (
     <>
-      <h1 className="bg-dark text-white text-center my-4 py-4">React Wiki</h1>
-      <p className="alert alert-warning">
-        <b>
-          Esta Wiki esta pensada para gente que ya posee cierto conocimiento con
-          React
-        </b>{" "}
-        pero que por diferentes motivos se han olvidado de algunas de sus
-        funciones o necesita reforzarlas.
-      </p>
-      <h2 className="text-center mb-5">Introducción</h2>
-      <h5 className="text-center mb-5">
-        Aquí encontraremos varios ejemplos para saber utilizar las principales
-        ventajas de React.
-        <br />
-        <br />
-        Ten presente que por motivos de incompatibilidad se han tenido que
-        eliminar las llavecillas de los cuadros de codigos de ejemplos y no es
-        completamente fiel a como realmente hay que escribirlo.
-        <br />
-        <br />
-        Si lo necesitas siempre puedes recurrir al código original en:
-        <br />
-        <br />
+      <h1 className="bg-dark text-white text-center my-4 py-4">{t('reactWiki')}</h1>
+
+      <p className="alert alert-warning" dangerouslySetInnerHTML={{__html: t('warningBox')}}/>
+      <h2 className="text-center mb-5">{t('introduction')}</h2>
+      <h5 className="text-center" dangerouslySetInnerHTML={{__html: t('introductionDetails')}}/>
+      <div className="text-center mb-5 fs-5">
         <ul className="d-inline-block">
           <li>
             <a
@@ -31,7 +18,7 @@ const HomePage = () => {
               target="_blank"
               rel="noreferrer"
             >
-              GitHub
+              {t('gitHub')}
             </a>
           </li>
           <li>
@@ -40,55 +27,15 @@ const HomePage = () => {
               target="_blank"
               rel="noreferrer"
             >
-              Sandbox
+              {t('sandBox')}
             </a>
           </li>
         </ul>
-      </h5>
+      </div>
 
       <div className="alert alert-secondary">
-        <h3 className="mb-4">¿Que es React?</h3>
-        <p>
-          La tecnología "React", también conocida como "React.js" o simplemente
-          "React", es una biblioteca de JavaScript desarrollada por Facebook. Se
-          utiliza para construir interfaces de usuario interactivas y
-          reutilizables en aplicaciones web. React se centra en la creación de
-          componentes reutilizables que representan diferentes partes de la
-          interfaz de usuario y su estado.
-          <br />
-          <br />
-          La principal característica de React es su enfoque en la construcción
-          de interfaces de usuario mediante la utilización de componentes. Un
-          componente en React es una unidad independiente que puede contener su
-          propia lógica y estado, y puede renderizarse en la interfaz de usuario
-          de acuerdo con las actualizaciones del estado. Esto permite la
-          creación de interfaces de usuario modulares y escalables.
-          <br />
-          <br />
-          React utiliza un modelo de programación llamado "Virtual DOM" (DOM
-          Virtual), que es una representación en memoria de la estructura actual
-          de la interfaz de usuario. Cuando el estado de un componente cambia,
-          React genera una actualización virtual del DOM en lugar de manipular
-          directamente el DOM del navegador. Luego, compara esta actualización
-          virtual con la versión anterior para determinar los cambios reales que
-          deben aplicarse al DOM real. Esta técnica de optimización ayuda a
-          mejorar el rendimiento y la eficiencia de las aplicaciones.
-          <br />
-          <br />
-          React es muy popular en el desarrollo web moderno y se combina a
-          menudo con otras bibliotecas y herramientas para construir
-          aplicaciones completas. Para administrar el estado de las aplicaciones
-          en React, se pueden usar soluciones como "Redux" o "Mobx". Además,
-          React puede integrarse con marcos de trabajo de desarrollo web como
-          "React Router" para manejar la navegación y "Axios" para realizar
-          solicitudes HTTP.
-          <br />
-          <br />
-          En resumen, React es una tecnología que permite construir interfaces
-          de usuario interactivas y dinámicas en aplicaciones web a través de
-          componentes reutilizables y un enfoque eficiente en la manipulación
-          del DOM.
-        </p>
+        <h3 className="mb-4">{t('infoBoxTitle')}</h3>
+        <p dangerouslySetInnerHTML={{__html: t('infoBox')}}/>
       </div>
     </>
   );
